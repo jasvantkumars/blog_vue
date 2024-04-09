6fd22da6bc80e64a9bdb82c4796cfd2ce0231934
jasvantkumars <jasvantkumar9555@gmail.com>
2024-04-09 09:51:50 +0530

    code_flow

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="hello">
+      <h1>Home Page</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomE',
+   
+  }
+  </script>
+
+<style scoped>
+h1{
+  color: orange;
+}
+</style>
