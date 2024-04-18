364c4992f1cc774e6ecc02d7cae874091d6b7b4f
jasvantku <167051124+jasvantku@users.noreply.github.com>
2024-04-18 10:53:35 +0530

    init

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,42 +1,27 @@
 <template>
-  <h1>Get Checkbox & Radio Button Value</h1>
+  <h1 v-if="show">If Condtion</h1>
 
-  <h3>Technology</h3>
-  <label for="java">Java</label>
+  <h1 v-else>else Condtion</h1>
 
-  <input type="checkbox" value="java" v-model="technology" id="java">
-
-  <label for="php">Php</label>
-  <input type="checkbox" value="php" v-model="technology" id="php">
-
-  <label for="node">Node</label>
-  <input type="checkbox" value="node" v-model="technology" id="node">
-
-  <br /><br />
-  <h3>Who I am</h3>
-
-  <label for="student">Student</label>
-  <input type="radio" value="student"  v-model="who" name="who" id="student">
-
-  <label for="developer">Developer</label>
-  <input type="radio" value="developer"  v-model="who" name="who" id="developer">
-  I
-
-  <br /><br />
-  <h4>Selected Technology: {{ technology }}</h4>
-  <h4>I am a: {{ who }}</h4>
-
+  <button v-on:click="show = !show">Toogle</button>
 </template>
-  
-  
-  <script>
-  export default {
-    data() {
-      return {
-        technology: [],
-        who: null
-      }
+
+<script>
+export default {
+  name: "HomeComponent",
+  data() {
+
+    return {
+      show: true
+
     }
+
   }
-  </script>
-  
+}
+</script>
+
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
